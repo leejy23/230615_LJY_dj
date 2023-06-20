a9b22807bd2705dc12022b7845411ec71b09d13e
Lee jae young <105205327+leejy23@users.noreply.github.com>
2023-06-20 20:13:31 +0900

    2023_06_20
    캐릭터 입히기 및 이동

---
+++ 230615_LJY/Assets/Scripts/MoveObject.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+   int change = 0;
+    
+    void Update()
+    {   
+        float MoveY = this.transform.position.y;
+        float MoveX = this.transform.position.x;
+        
+        if(MoveY >= 2f)
+        {
+            change =1;
+        }
+        else if(MoveX >= 2f && MoveY>=2f)
+        {
+            change =2;
+        }else if(MoveY<= -2 && MoveX>= 2){
+            change =3;
+        }else if (MoveX <=-2 && MoveY<= -2){
+            change =4;
+        }
+
+        if(change == 0)
+        {
+            this.transform.position = this.transform.position + new Vector3(0,2 * Time.deltaTime,0);
+        }
+        else if(change == 1)
+        {
+            this.transform.position = this.transform.position + new Vector3(2 * Time.deltaTime,0,0);
+        }
+        else if(change == 2)
+        {
+            this.transform.position = this.transform.position + new Vector3(0,-2 * Time.deltaTime,0);
+        }
+        else if(change == 3)
+        {
+            this.transform.position = this.transform.position + new Vector3(-2 * Time.deltaTime,0,0);
+        }
+        
+        if(change ==4){
+            change = 0;
+        }
+
+    }
+}
